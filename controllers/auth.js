7de0c795db0fa4d82312a70f0e7a906d1141449f
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-04-18 00:17:56 -0300

    creating user on database

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,41 @@
 const { response, request } = require("express");
+const Usuario = require("../models/Usuario");
 
-const crearUsuario = (req = request, res = response) => {
+const crearUsuario = async (req = request, res = response) => {
   const { email, name, password } = req.body;
 
-  return res.json({
-    ok: true,
-    msg: "Crear usuario /new",
-  });
+  try {
+    //Verificar el email
+    const usuario = await Usuario.findOne({ email });
+    if (usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El usuario ya existe",
+      });
+    }
+    //Crear usuario con el modelo
+    const dbUser = new Usuario(req.body);
+
+    //Hashear el password
+
+    //Generar un token JWT
+
+    //Crear usuario de DB
+    await dbUser.save();
+
+    //Generar respuesta exitosa
+    return res.status(201).json({
+      ok: true,
+      uid: dbUser.id,
+      name,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el administrador",
+    });
+  }
 };
 
 const loginUsuario = (req = request, res = response) => {
