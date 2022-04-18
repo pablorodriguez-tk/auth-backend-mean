3435f9eabd72a5e8ab0afd76da0233e1bdec3887
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-04-17 22:07:30 -0300

    routes

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hola desde node");
+const express = require("express");
+
+//Crear el servidor express
+const app = express();
+
+//Rutas
+app.use("/api/auth", require("./routes/auth"));
+
+//Levantar servidor express
+app.listen(4000, () => {
+  console.log(`Servidor corriendo en el puerto ${4000}`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,29 @@
+const { Router } = require("express");
+
+const router = Router();
+
+//Crear un usuario
+router.post("/new", (req, res) => {
+  return res.json({
+    ok: true,
+    msg: "Crear usuario /new",
+  });
+});
+
+//Login de usuario
+router.post("/", (req, res) => {
+  return res.json({
+    ok: true,
+    msg: "Login de usuario /",
+  });
+});
+
+//Validar y revalidar token
+router.get("/renew", (req, res) => {
+  return res.json({
+    ok: true,
+    msg: "Renew",
+  });
+});
+
+module.exports = router;
