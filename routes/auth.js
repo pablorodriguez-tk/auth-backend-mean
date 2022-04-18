b26b9b30b1135c5924030885742140d3743e9033
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-04-17 22:13:44 -0300

    controllers

--- routes/auth.js
+++ routes/auth.js
@@ -1,29 +1,19 @@
 const { Router } = require("express");
+const {
+  crearUsuario,
+  revalidarToken,
+  loginUsuario,
+} = require("../controllers/auth");
 
 const router = Router();
 
 //Crear un usuario
-router.post("/new", (req, res) => {
-  return res.json({
-    ok: true,
-    msg: "Crear usuario /new",
-  });
-});
+router.post("/new", crearUsuario);
 
 //Login de usuario
-router.post("/", (req, res) => {
-  return res.json({
-    ok: true,
-    msg: "Login de usuario /",
-  });
-});
+router.post("/", loginUsuario);
 
 //Validar y revalidar token
-router.get("/renew", (req, res) => {
-  return res.json({
-    ok: true,
-    msg: "Renew",
-  });
-});
+router.get("/renew", revalidarToken);
 
 module.exports = router;
