6baab0c345123ecda449ab7f4d6c9fa6a3f6e1eb
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-04-17 23:40:08 -0300

    custom middleware for validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ const { response, request } = require("express");
 
 const crearUsuario = (req = request, res = response) => {
   const { email, name, password } = req.body;
-  console.log(email, name, password);
+
   return res.json({
     ok: true,
     msg: "Crear usuario /new",
@@ -11,7 +11,7 @@ const crearUsuario = (req = request, res = response) => {
 
 const loginUsuario = (req = request, res = response) => {
   const { email, password } = req.body;
-  console.log(email, password);
+
   return res.json({
     ok: true,
     msg: "Login de usuario /",
--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,36 @@
 const { Router } = require("express");
+const { check } = require("express-validator");
 const {
   crearUsuario,
   revalidarToken,
   loginUsuario,
 } = require("../controllers/auth");
+const { validarCampos } = require("../middlewares/validar-campos");
 
 const router = Router();
 
 //Crear un usuario
-router.post("/new", crearUsuario);
+router.post(
+  "/new",
+  [
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "El password es obligatorio").isLength(6),
+    validarCampos,
+  ],
+  crearUsuario
+);
 
 //Login de usuario
-router.post("/", loginUsuario);
+router.post(
+  "/",
+  [
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "El password es obligatorio").isLength(6),
+    validarCampos,
+  ],
+  loginUsuario
+);
 
 //Validar y revalidar token
 router.get("/renew", revalidarToken);
